0a986be20e6f7c99e89535d1a68c52893976875a
Jomon Johnson <jomon.entero@gmail.com>
2019-07-08 13:48:18 +0530

    rest api

---
+++ config/moduleApi.php
@@ -0,0 +1,33 @@
+<?php
+/* @var $_api array */
+
+use codexten\yii\modules\api\ApiModule;
+
+return [
+    'modules' => [
+        'api' => [
+            'class' => ApiModule::class,
+        ],
+    ],
+    'components' => [
+        'urlManager' => [
+            'rules' => [
+                'api' => [
+                    'class' => 'yii\rest\UrlRule',
+                    'patterns' => [
+                        'POST {id}' => 'update',
+                        'DELETE {id}' => 'delete',
+                        'GET,HEAD {id}' => 'view',
+                        'POST' => 'create',
+                        'GET,HEAD' => 'index',
+                        '{id}' => 'options',
+                        '' => 'options',
+                    ],
+                    'controller' => [
+
+                    ],
+                ],
+            ],
+        ],
+    ],
+];
---
+++ src/ApiModule.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace codexten\yii\modules\api;
+
+
+use codexten\yii\base\Module;
+use yii\helpers\ArrayHelper;
+use yii\web\UrlManager;
+
+class ApiModule extends Module
+{
+
+}
